67ba4ba5c5c1154e8e50f5d41c971d5664bfd65f
Miroslav Horák <warcallerthefirst@gmail.com>
2021-07-16 13:52:14 +0200

    Fix bug in chatbot's variables

--- archiemate.com/chatbot/ArchieMate/Variables.py
+++ archiemate.com/chatbot/ArchieMate/Variables.py
@@ -2,7 +2,7 @@ from typing import Dict
 
 class Variables:
   def __init__(self, json):
-    self.variables: Dict[int, Dict[int, str]] = {int(channel): {variable: json[channel][variable] for variable in json} for channel in json}
+    self.variables: Dict[int, Dict[int, str]] = {int(channel): {variable: json[channel][variable] for variable in json[channel]} for channel in json}
   
   def get_variables(self, channel: int) -> Dict[str, str]:
     if channel not in self.variables:
