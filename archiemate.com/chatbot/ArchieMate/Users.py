5085dd859001336ee100927a95535bc6aeb2e633
Miroslav Horák <warcallerthefirst@gmail.com>
2021-07-16 14:24:23 +0200

    Make all class atributes public in Users module

--- archiemate.com/chatbot/ArchieMate/Users.py
+++ archiemate.com/chatbot/ArchieMate/Users.py
@@ -12,16 +12,16 @@ class Channel:
   def __init__(self, json: Optional[Dict[str, Any]] = None):
     self.points: int = int(json["points"]) if json is not None and "points" in json else 0
     self.mod: bool = json["mod"] if json is not None and "mod" in json else False
-    self.__commands: Dict[str, datetime.datetime] = {
+    self.commands: Dict[str, datetime.datetime] = {
       command: datetime.datetime.utcfromtimestamp(timestamp).replace(microsend=timestamp % (1000 * 1000))
       for command in json["commands"]
       if (timestamp := int(json["commands"][command]))
     } if json is not None and "commands" in json else {}
   
   def get_command(self, command: str) -> Dict[str, datetime.datetime]:
-    if command not in self.__commands:
-      self.__commands[command] = datetime.datetime.utcfromtimestamp(0)
-    return self.__commands[command]
+    if command not in self.commands:
+      self.commands[command] = datetime.datetime.utcfromtimestamp(0)
+    return self.commands[command]
 
 class User:
   @staticmethod
@@ -32,14 +32,14 @@ class User:
     self.user: str = json["user"] if json is not None and "user" in json else kwargs["user"]
     self.display_name: str = json["display_name"] if json is not None and "display_name" in json else kwargs["display_name"]
     self.bot: bool = json["bot"] if json is not None and "bot" in json else kwargs["bot"]
-    self.__channels: Dict[int, Channel] = {
+    self.channels: Dict[int, Channel] = {
       channel: Channel(json["channels"][channel]) for channel in json["channels"]
     } if json is not None and "channels" in json else {}
   
   def get_channel(self, id: int) -> Channel:
-    if id not in self.__channels:
-      self.__channels[id] = Channel.create_new(id)
-    return self.__channels[id]
+    if id not in self.channels:
+      self.channels[id] = Channel.create_new(id)
+    return self.channels[id]
 
 class Users:
   def __init__(self, json):
@@ -57,11 +57,11 @@ class Users:
             "mod": channel_detail.mod,
             "commands": {
               command: command_detail.timestamp()
-              for command in channel_detail.__commands
-              if (command_detail := channel_detail.__commands[command])
+              for command in channel_detail.commands
+              if (command_detail := channel_detail.commands[command])
             }
-          } for channel in user_detail.__channels
-          if (channel_detail := user_detail.__channels[channel])
+          } for channel in user_detail.channels
+          if (channel_detail := user_detail.channels[channel])
         }
       } for user in self.users
       if (user_detail := self.users[user])
