de55672e65b3f488e99a08f5fe3d1dabd3c79235
Miroslav Horak <warcallerthefirst@gmail.com>
2021-07-20 16:36:34 +0200

    Add debug to SocketServer

--- archiemate.com/chatbot/ArchieMate/SocketServer.py
+++ archiemate.com/chatbot/ArchieMate/SocketServer.py
@@ -23,6 +23,7 @@ class SocketServer:
     state: Dict[str, Any]
   
   def __init__(self, poller: Poller.Poller):
+    logger.debug(f"SocketServer.__init__(poller: {poller})")
     self.socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.socket.bind(('127.0.0.1', int(env.get("ARCHIEMATE_SOCKET_SERVER_PORT"))))
     self.socket.listen()
@@ -30,14 +31,19 @@ class SocketServer:
     self.raw_sockets: queue.Queue = queue.Queue()
     self.sockets: List[SocketServer.SocketInfo] = []
     self.poller.add_socket(self.socket, server=True, server_sockets=self.raw_sockets)
+    logger.debug(f"Result: {self.__dict__}")
   
   def check_sockets(self) -> None:
+    logger.debug("SocketServer.check_sockets()")
     while self.raw_sockets.qsize() > 0:
       new_socket: socket.socket = self.raw_sockets.get(0)
+      logger.debug(f"new socket {new_socket} - something connected!")
       self.sockets.append(SocketServer.SocketInfo(new_socket, SocketType.Unknown, {}))
       dead_sockets = [socket for socket in self.sockets if socket.state.get("dead") == True]
       for dead_socket in dead_sockets:
+        logger.debug(f"removing dead socket {dead_socket.__dict__}")
         self.sockets.remove(dead_socket)
+    logger.debug(f"Result: {self.__dict__}")
   
   def __del__(self):
     del self.socket
